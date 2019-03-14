5cbce4c7ac6172db3b616a237518398878a32d05
Moonpeth <moonpeth@gmail.com>
2019-03-14 16:16:34 +0100

    updated demo to show toast with inner html

--- src/react-toasts/index.js
+++ src/react-toasts/index.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import {ToastsContainer, ToastsStore, ToastsContainerPosition} from 'react-toasts';
+import {ToastsContainer, ToastsStore, ToastsContainerPosition} from '../../../react-toasts/lib';
 
 import './index.css';
 
@@ -29,6 +29,36 @@ export const ReactToasts = () => {
                     <div className="button buttonError" onClick={() => {
                         ToastsStore.error("Haaa it doesn't work anymore x(\nThis toast has custom bootstrap classes", 5000, 'alert-danger')
                     }}>Error
+                    </div>
+                    <div className="button buttonHtml" onClick={() => {
+                        ToastsStore.error(<div><h1>Title element</h1>
+                            <p>
+                                <svg enableBackground="new 0 0 64 64" id="Layer_1" version="1.1" viewBox="0 0 64 64">
+                                    <circle cx="32" cy="32" fill="#FFD766" r="24"/>
+                                    <path d="M48.7,38.2C46,44.8,39.6,49.5,32,49.5c-7.6,0-14.1-4.7-16.7-11.4L48.7,38.2z"
+                                          fill="#4B687F"/>
+                                    <path
+                                        d="M25.5,48.2c2,0.8,4.2,1.2,6.6,1.2c2.3,0,4.5-0.4,6.5-1.2c0-3.6-2.9-6.5-6.5-6.5  C28.4,41.8,25.5,44.7,25.5,48.2z"
+                                        fill="#EA685E"/>
+                                    <path
+                                        d="  M48.7,38.2C46,44.8,39.6,49.5,32,49.5c-7.6,0-14.1-4.7-16.7-11.4L48.7,38.2z"
+                                        fill="none" stroke="#2C3E50"
+                                        strokeLinejoin="round" strokeMiterlimit="10" strokeWidth="2"/>
+                                    <circle cx="41" cy="25.2" fill="#FFFFFF" r="3.8"/>
+                                    <circle cx="23" cy="25.2" fill="#FFFFFF" r="3.8"/>
+                                    <circle cx="41" cy="25.2" fill="none" r="3.8" stroke="#2C3E50"
+                                            strokeLinejoin="round"
+                                            strokeMiterlimit="10" strokeWidth="2"/>
+                                    <circle cx="23" cy="25.2" fill="none" r="3.8" stroke="#2C3E50"
+                                            strokeLinejoin="round"
+                                            strokeMiterlimit="10" strokeWidth="2"/>
+                                    <circle cx="32" cy="32" fill="none" r="24" stroke="#2C3E50" strokeLinejoin="round"
+                                            strokeMiterlimit="10" strokeWidth="2"/>
+                                </svg>
+                                Paragraph element with an SVG inside
+                            </p>
+                        </div>, 15000, 'alert-danger')
+                    }}>HTML Content
                     </div>
                 </div>
             </div>
